5fa2f5bf01ab976005f5936d652a0c48c0dc7de0
Jack Taylor <77901413+jahreja@users.noreply.github.com>
2021-02-22 22:46:56 +0000

    fgf

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express')
 var app = express()
 
-const P0RT = process.env.P0RT || 8000;
 
 app.use(express.static('www'));
 
-var server = app.listen(P0RT, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
     var host = server.address().address
     var port = server.address().port
